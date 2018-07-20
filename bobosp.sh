9609b73bb00032b95011845261b694b6f914da7f
cpjt <40307004+cpjt@users.noreply.github.com>
2018-07-20 16:41:17 -0300

    Update bobosp.sh

--- bobosp.sh
+++ bobosp.sh
@@ -9,11 +9,11 @@ FPS="15"                                       # FPS de la vidéo en sortie
 QUAL="low"                                  # Preset de qualité FFMPEG
 YOUTUBE_URL="rtmp://104.196.20.47:1935/live"  # URL de base RTMP youtube
 
-SOURCE="http://89.163.251.186:8081/loadbalance/sptv/chunks.m3u8"              # Source UDP (voir les annonces SAP)
+SOURCE="http://www.blogger.com/video-play.mp4?contentId=cc692bc9c5f4200d"              # Source UDP (voir les annonces SAP)
 KEY="bob"                                     # Clé à récupérer sur l'event youtube
 
 ffmpeg \
     -i "$SOURCE" -deinterlace \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -vcodec libx264 -pix_fmt yuv420p -preset ultrafast -b 900k -s 320x240 -r 15 -g 15 -b:v 200k \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
     -f flv "$YOUTUBE_URL/$KEY"
