9be61d94f332ebe2ac2ebf60d2ee4389fee78e3f
cpjt <40307004+cpjt@users.noreply.github.com>
2018-07-20 16:09:28 -0300

    Update bobosp.sh

--- bobosp.sh
+++ bobosp.sh
@@ -1,19 +1,8 @@
-#! /bin/bash
-#
-# Diffusion youtube avec ffmpeg
-
-# Configurer youtube avec une résolution 720p. La vidéo n'est pas scalée.
-
-VBR="200k"                                    # Bitrate de la vidéo en sortie
-FPS="15"                                       # FPS de la vidéo en sortie
-QUAL="medium"                                  # Preset de qualité FFMPEG
-YOUTUBE_URL="rtmp://104.196.20.47:1935/live"  # URL de base RTMP youtube
-
-SOURCE="http://89.163.251.186:8081/loadbalance/sptv/chunks.m3u8"  # Source UDP (voir les annonces SAP)
-KEY="filmes"                                     # Clé à récupérer sur l'event youtube
+RTMP="rtmp://104.196.20.47:1935/live"
+SOURCE="http://89.163.251.186:8081/loadbalance/sptv/chunks.m3u8"
+KEY="filmes"
 
 ffmpeg \
     -i "$SOURCE" -deinterlace \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 1)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
-    -f flv "$YOUTUBE_URL/$KEY"
+ffmpeg -re -f lavfi -i testsrc -c:v libx264 -b:v 200k -preset ultrafast -b 900k -c:a libfdk_aac -b:a 96k -s 320x240 -x264opts keyint=50 -g 15 -pix_fmt yuv420p \
+    -f flv "$RTMP/$KEY"
